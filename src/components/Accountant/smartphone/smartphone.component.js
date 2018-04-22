77cb736323740fd7a50d4039e96eca873aaa292b
Dang Chi Son <kenshin@Shinigami.net.fpt>
2018-04-22 09:01:22 +0700

    Fixed show data from smartphone

--- src/components/Accountant/smartphone/smartphone.component.js
+++ src/components/Accountant/smartphone/smartphone.component.js
@@ -16,7 +16,7 @@ export default {
   },
   computed: {
     ...mapGetters([
-      'coins', 'statistics', 'sumTotal'
+      'coins', 'statistics'
     ]),
     isRemove() {
       return this.statistics.length > 1
@@ -28,12 +28,58 @@ export default {
   methods: {
     ...mapActions([
       'addCoin',
-      'removeCoin',
-      'coinTotalCurrent',
-      'coinTotalBuy'
+      'removeCoin'
     ]),
     islose (coin) {
       return coin.total_current < coin.total_buy
+    },
+    total (coin, type) {
+      let _total = '-'
+
+      switch (type) {
+        case 'buy':
+          if (coin.price_buy && coin.amount) {
+            _total = coin.amount * coin.price_buy
+            coin.total_buy = _total
+          }
+          break;
+        case 'current':
+          if (coin.price_usd && coin.amount) {
+            _total = coin.amount * coin.price_usd
+            coin.total_current = _total
+          }
+          break;
+      }
+
+      return _total
+    },
+    profitLoss (coin) {
+      if (coin.total_current) {
+        this.sumCoin()
+
+        if (coin.total_buy) {
+          let _profitLoss = coin.total_current - coin.total_buy
+
+          if (_profitLoss > 0) {
+            coin.textProfitLoss = 'trending_up'
+          } else {
+            coin.textProfitLoss = 'trending_down'
+          }
+
+          return _profitLoss
+        }
+      }
+    },
+    sumCoin () {
+      let _total = 0
+
+      this.statistics.forEach(item => {
+        if (item.coin.total_current) {
+          _total += item.coin.total_current
+        }
+      })
+
+      this.sumTotal = _total
     }
   }
 }
